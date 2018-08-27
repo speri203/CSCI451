2bbd84475ed81fad3d647e8151e43a2829f46260
Sai Peri <peri@Sais-MacBook-Pro.local>
2018-08-27 13:09:55 -0500

    Added piping system output to file

--- hw1.c
+++ hw1.c
@@ -1,20 +1,29 @@
+/*
+ *	Sai Peri
+ *	Homework 1
+ *	8/27/2018
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int main(int argc, char *argv[]) { /*argc holds the number of arguements and argv is an array of string pointers with indifinate size  */
 
 	/*Check to see if no more than 4 arg entered */
-	if(argc > 4 && argc > 0) {
+	if(argc > 5 && argc > 0) {
 		printf("Invalid number of arguements. No greater than 4");
 		return 0;
 	}
-	FILE *fp;
+	FILE *fp; // only creating file pointer so the output file is rewritten each runtime. Otherwise it appends
 	int i;
-	
-	fp = fopen("output.txt","w");
-	/*writing to a file is fprintf()*/
+	char temp[128]; // char array in order to append output.
+
+	fp = fopen("output.txt", "w"); //Rewriting to file when command runs
+
 	for(i = 1; i < argc; i++) {
-		fprintf(fp, "%s", argv[i]); /* Remember to specify type of what is being returned*/
+		sprintf(temp, "%s >> output.txt", argv[i]); //Appending piping (C right-shift operator?)
+		system(temp); // System command in order to run queries
 	}
-	fclose(fp);
+	fclose(fp); // Closing file
 	return 0;
 }
