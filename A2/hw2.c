25ebe5b590abc5a39f5f574c193a26d0f6d95041
speri203 <peri.sai203@gmail.com>
2018-08-30 00:02:30 -0500

    Fork bomb source code

---
+++ A2/hw2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+
+  printf("The process id is: %d\n", (int)getpid());
+  char *Command = "X\n";
+  pid_t pid = fork();
+  if(pid < 0) {
+    perror("FORK FAILED.\n");
+    return 0;
+  }
+  if(pid == 0) { //Child code execution goes here.
+    printf("CHILD: Process ID: %d\n", (int)getpid());
+    system("xclock");
+    execv(Command, NULL);
+    main();
+    exit(0);
+  }
+
+  /*
+    Below this block is parent or pid_t > 1 case
+  */
+  printf("PARENT: Process ID: %d\n", (int)getpid());
+  wait(NULL); // Requesting child process to be complete
+  return 0;
+}
